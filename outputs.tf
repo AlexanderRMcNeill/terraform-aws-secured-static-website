f1a9cd7311ecdf0ab715f04c078d4ea398643a4a
AlexanderRMcNeill <alexander.r.mcneill@gmail.com>
2018-05-05 20:03:09 +1200

    Add descriptions to the module's outputs

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "bucket_id" {
-    value = "${aws_s3_bucket.website_bucket.id}"
+  description = "The name of the created bucket."
+  value       = "${aws_s3_bucket.website_bucket.id}"
 }
 
 output "bucket_arn" {
-    value = "${aws_s3_bucket.website_bucket.arn}"
+  description = "The ARN (Amazon Resource Name) of the created bucket. Will be of format arn:aws:s3:::bucketname."
+  value       = "${aws_s3_bucket.website_bucket.arn}"
 }
 
 output "bucket_domain_name" {
-    value = "${aws_s3_bucket.website_bucket.bucket_domain_name}"
+  description = "The global domain name of the created bucket. Will be of format bucketname.s3.amazonaws.com."
+  value       = "${aws_s3_bucket.website_bucket.bucket_domain_name}"
 }
 
 output "distribution_id" {
-    value = "${aws_cloudfront_distribution.website_distribution.id}"
+  description = "The identifier for the created distribution. For example: EDFDVBD632BHDS5."
+  value       = "${aws_cloudfront_distribution.website_distribution.id}"
 }
 
 output "distribution_arn" {
-    value = "${aws_cloudfront_distribution.website_distribution.arn}"    
+  description = "The ARN (Amazon Resource Name) for the created distribution. For example: arn:aws:cloudfront::123456789012:distribution/EDFDVBD632BHDS5, where 123456789012 is your AWS account ID."
+  value       = "${aws_cloudfront_distribution.website_distribution.arn}"
 }
 
 output "distribution_domain_name" {
-    value = "${aws_cloudfront_distribution.website_distribution.domain_name}"
+  description = "The domain name corresponding to the created distribution. For example: d604721fxaaqy9.cloudfront.net."
+  value       = "${aws_cloudfront_distribution.website_distribution.domain_name}"
 }
